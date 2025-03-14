a029a2de7a8e0e89213c65ac89d0ca8496dd7b10
sebastian <trillossebastian148@gmail.com>
2025-03-14 17:02:13 -0500

    se agrega el archivo para terminar

---
+++ src/Numero.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+public class numero {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese el primer numero");
+        int inicio = input.nextInt();
+        System.out.println("ingrese el segundo numero");
+        int fin = input.nextInt();
+        System.out.println("el primer numero es " + inicio + " y el segundo es " + fin);
+        
+    
+    }
+}
