e419d21981f1ccede171f11458a74f1b67947cbe
sebastian trillos <trillossebastian148@gmail.com>
2025-03-26 20:18:13 -0500

    se termina el archivo Matriz.java

--- src/Matriz.java
+++ src/Matriz.java
@@ -6,24 +6,38 @@ public class Matriz {
 
         Scanner input = new Scanner(System.in);
 
+        int suma = 0;
+
         System.out.println("ingrese el numero de filas: ");
         int filas = input.nextInt();
         System.out.println("ingrese el numero de columnas: ");
         int columnas = input.nextInt();
         int[][] matriz = new int[filas][columnas];
 
+        int totalnumeros = filas * columnas;
+
         System.out.println("ingrese los valores de la matriz:");
-        for (int i = 1; i < filas; i++) {
-          for (int j = 1; j < columnas; j++) {
+        for (int i = 0; i < filas; i++) {
+          for (int j = 0; j < columnas; j++) {
               System.out.println("elemento [" + i + "][" + j + "]: ");
               matriz[i][j] = input.nextInt();
+             suma += matriz[i][j];
           }  
         }
-        for (int i = 1; i < filas; i++) {
-        for (int j = 1; j < columnas; j++) {
-            System.out.println(matriz[i][j] + " ");
+        System.out.println("la matriz es:");
+        for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            System.out.print(matriz[i][j] + " ");
         }
-            }
+        System.out.println();
+      }
+      input.close();
+
+      double promedio = suma / totalnumeros;
+
+      System.out.println("suma de los elementos de la matriz: " + suma);
+      System.out.println("promedio de los elementos: " + promedio);
+      System.out.println("cantidad de valores de la matriz: " + totalnumeros);
      }
 }
 
